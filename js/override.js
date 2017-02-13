61d0cea01578d6555054994eb72c091fb8ae5d1e
caubalejopaul96 <caubalejopaul96@gmail.com>
2017-02-13 13:57:08 +0800

    update_ver_1.0
    added new contents

--- js/override.js
+++ js/override.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+    $('.modal').modal();
   })
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.nav-bar').addClass('show-background');
+    } else {
+        $('.nav-bar').removeClass('show-background');
+    }
+});
